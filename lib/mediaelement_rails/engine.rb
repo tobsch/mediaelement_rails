37b0f897b2c446b8cf7318d0dfd0adac4f7407d4
Kevin Elliott <kevin@welikeinc.com>
2011-12-09 05:06:22 -0800

    Initialize after sprockets has been

--- lib/mediaelement_rails/engine.rb
+++ lib/mediaelement_rails/engine.rb
@@ -3,7 +3,7 @@ require "jquery-rails"
 
 module MediaelementRails
   class Engine < Rails::Engine
-    config.after_initialize do |app|
+    initializer 'mediaelement_rails', :after => "sprockets.environment" do |app|
       next unless app.assets
       app.assets.register_mime_type "application/x-silverlight-app", ".xap"
     end
